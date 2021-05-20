14fc09b6a8d725518c6137709928e0decf9ef96a
Rigoberto Juarez jr <rigoberto.juarez.48@my.csun.edu>
2021-05-19 18:15:14 -0700

    fixed script

---
+++ client/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+import Auth from '../Warehouse/Auth';
+
+export default createStore({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    Auth
+  }
+})
